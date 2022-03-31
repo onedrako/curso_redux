86ddcefdae7818bd9101ee8b7e67e3328ac7cb17
onedrako <onedrako@gmail.com>
2022-03-31 08:48:39 -0600

    Class 10 working, middlewares

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './containers/App'
-import { createStore } from 'redux'
+import { createStore, compose, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import './index.css'
 
 import pokemonReducer from './reducers/pokemonReducer'
+import { logActions } from './middlewares'
 
-const store = createStore(pokemonReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const composedEnhacers = compose(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(logActions))
+
+const store = createStore(pokemonReducer, composedEnhacers)
 
 ReactDOM.render(
 <Provider store={store}>
